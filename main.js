f7b9e23f1824ebf31c59547e1428e09577989d77
akmalxon1 <akmalxon359@gmail.com>
2024-08-21 23:28:03 +0500

    hello bro

---
+++ main.js
@@ -0,0 +1,18 @@
+let body = document.querySelector("body")
+
+let div = document.createElement("div")
+
+let h1 = document.createElement("h1")
+
+div.appendChild(h1)
+h1.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus culpa incidunt quas magnam, sit autem commodi mollitia velit recusandae vel libero perferendis voluptate optio! Porro natus sit quae maiores rem blanditiis asperiores vitae molestiae pariatur nam aspernatur delectus fugiat hic laboriosam corporis dolor cumque totam, architecto inventore? Nemo tempore vel architecto delectus porro cupiditate, quia et, totam molestiae aspernatur velit fugit eligendi neque saepe amet eveniet voluptatem nisi asperiores nobis modi dolorum, quam consectetur. Ad facilis sunt velit minus, repellendus vero iste! Deleniti, adipisci! Pariatur odit sequi, veniam vero quo reiciendis ipsum quae neque necessitatibus. Vel adipisci eum corporis mollitia id, doloremque vitae non maxime sunt ad accusantium, eos at repellat rem. Error dolorum ratione saepe nesciunt, odio ad exercitationem beatae explicabo! Modi dolorem doloribus deleniti, magni possimus vero nesciunt tenetur ipsum sed illo amet maxime nulla? Nobis eveniet, explicabo corrupti quos aperiam placeat ab rem, ipsum ut magnam eos obcaecati veniam temporibus id quasi deleniti quae magni, sunt itaque minima asperiores culpa! Voluptatum libero nemo unde similique doloribus earum obcaecati error nobis fugit! Dicta ad omnis sapiente dolor aspernatur. Laboriosam, at excepturi! Alias, provident corporis illum quidem facilis officiis distinctio molestiae sit culpa quia ipsam ipsa laudantium nemo voluptas accusamus quaerat explicabo odio perspiciatis reiciendis cupiditate quam at nulla fugiat. Excepturi reprehenderit reiciendis, soluta illum placeat maiores voluptates nesciunt mollitia deserunt incidunt cum aspernatur nostrum hic itaque explicabo dolorum velit magni? Et, tenetur expedita aliquam consequuntur molestias quisquam? Est, quia doloremque voluptatum inventore vitae animi officia totam molestias sequi debitis, repellat consectetur non qui incidunt sapiente alias illo harum quasi dolorem odio eligendi adipisci perferendis. Illo, eius in consectetur excepturi labore vel rerum! Quia ullam totam adipisci sed ex error quae provident praesentium sunt nemo incidunt esse vel repudiandae, ad laborum fugiat. Quos quod doloremque dicta numquam itaque natus quis, ab tempore, inventore dolor quibusdam, incidunt repudiandae laborum laboriosam animi porro perferendis ad optio dolore. Numquam eligendi delectus tempore repudiandae voluptas enim, nemo, cupiditate qui at excepturi libero facere ducimus in sint non! Libero, adipisci nesciunt, modi beatae necessitatibus commodi sint est ab nisi earum quo enim accusamus qui. Beatae commodi aspernatur facere laboriosam veniam deserunt praesentium accusamus rerum id illum nemo fuga autem inventore, mollitia excepturi non animi, voluptatum sint, enim libero? Ipsa voluptatem doloribus culpa quo numquam exercitationem! Doloribus facilis non odio beatae repellendus quo veniam, blanditiis repellat ab voluptatum impedit. Eum eligendi quia suscipit quaerat sunt!";
+
+body.appendChild(div)
+
+console.log(body);
+
+let a = prompt("backgroundColor kiriting");
+let b = prompt("textning rangini kriting");
+
+div.style.backgroundColor = a;
+h1.style.color = b;
